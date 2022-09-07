fce4bdc8777cd212622ba24ddb9f48bb8538b046
ahirlog <rhahir2015@gmail.com>
2022-09-07 23:25:13 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:country_details/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: const Color(0xff31352E),
+        primaryColorLight: const Color(0xff535E4B),
+        primaryColorDark: const Color(0xff503440),
+        accentColor: const Color(0xffAE9864),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
